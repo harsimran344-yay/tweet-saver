8f87777624265ef651a51854779deb259844e303
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-07-12 11:18:22 -0400

    add components in app js and commit node package

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import TweetsTable from "../TweetsTable/TweetsTable";
+import SavedTweets from "../SavedTweets/SavedTweets";
+import SearchBar from "../SearchBar/SearchBar"
+import TweetsContextProvider from "../../context/TweetsContext";
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const handleSubmit = (e, searchInput) => {
+    e.preventDefault();
+    setSearchText(searchInput)
+  }
   return (
     <div className="App">
-      test
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h1 className="tweet-saver-heading">
+        Tweet Saver
+      </h1>
+      <TweetsContextProvider>
+        <SearchBar className="search-button" handleSubmit={handleSubmit}/>
+        <p className="saved-tweets-text">Saved Tweets</p>
+        <TweetsTable searchTerm={searchText} />
+        <div className="drag-arrow">
+          Drag tweets
+          <div className="line"></div>
+          <div className="point"></div>
+          <p>to save</p>
+        </div>
+        <SavedTweets />
+      </TweetsContextProvider>
     </div>
   );
 }
